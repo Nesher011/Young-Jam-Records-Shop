f0cd87f66c07f0de3f0fe49f04baef6bfd9407c5
Jędrzej Kłyza <neshernos@gmail.com>
2022-09-28 02:33:58 +0200

    Add project files.

---
+++ Young Jam Records Shop/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Young_Jam_Records_Shop.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
