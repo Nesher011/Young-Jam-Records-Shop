5843edbc691d135e820872b8298fe428a702deb9
Jędrzej Kłyza <neshernos@gmail.com>
2022-10-02 20:26:31 +0200

    changed update method for product repository

--- YoungJamRecordsShop.DataAccess/Repository/ProductRepository.cs
+++ YoungJamRecordsShop.DataAccess/Repository/ProductRepository.cs
@@ -19,6 +19,25 @@ namespace YoungJamRecordsShop.DataAccess.Repository
 
         public void Update(Product obj)
         {
+            var objFromDb = _dbContext.Product.FirstOrDefault(u => u.Id == obj.Id);
+            if (objFromDb == null)
+            {
+                objFromDb.Author = obj.Author;
+                objFromDb.AlbumType = obj.AlbumType;
+                objFromDb.AlbumTypeId = obj.AlbumTypeId;
+                objFromDb.ListPrice = obj.ListPrice;
+                objFromDb.EAN = obj.EAN;
+                objFromDb.Price = obj.Price;
+                objFromDb.CaseType = obj.CaseType;
+                objFromDb.CaseTypeId = obj.CaseTypeId;
+                objFromDb.Description = obj.Description;
+                objFromDb.Title = obj.Title;
+
+                if (obj.CoverImageUrl != null)
+                {
+                    objFromDb.CoverImageUrl = obj.CoverImageUrl;
+                }
+            }
             _dbContext.Product.Update(obj);
         }
     }
