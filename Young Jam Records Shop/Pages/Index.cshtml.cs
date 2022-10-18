17b1fd69de04d23965a3e6b9b64b520962da48bb
Jędrzej Kłyza <neshernos@gmail.com>
2022-10-18 23:59:41 +0200

    Changed main page

--- Young Jam Records Shop/Pages/Index.cshtml.cs
+++ Young Jam Records Shop/Pages/Index.cshtml.cs
@@ -1,18 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using YoungJamRecordsShop.DataAccess.Repository.IRepository;
+using YoungJamRecordsShop.Models;
 
 namespace YoungJamRecordsShopWeb.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+        public IEnumerable<Product> ProductList { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public void OnGet()
         {
+            ProductList = _unitOfWork.Product.GetAll(includeProperties: "AlbumType,CaseType");
         }
     }
 }
